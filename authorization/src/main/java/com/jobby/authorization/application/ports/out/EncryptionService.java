6bbe5f576c6a6b2345fc1d06acc9aee1316b06d5
Amapola <htrsanbl@gmail.com>
2025-07-25 23:47:11 -0500

    fix: Resolve problems generated in under tests

--- authorization/src/main/java/com/jobby/authorization/application/ports/out/EncryptionService.java
+++ authorization/src/main/java/com/jobby/authorization/application/ports/out/EncryptionService.java
@@ -2,14 +2,8 @@ package com.jobby.authorization.application.ports.out;
 
 import com.jobby.authorization.domain.result.Error;
 import com.jobby.authorization.domain.result.Result;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 public interface EncryptionService {
-    public Result<String, Error> encrypt(String plainText);
-    public Result<String, Error> decrypt(String cipherText) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException;
+    public Result<String, Error> encrypt(String data, String keyBase64, int ivLength);
+    public Result<String, Error> decrypt(String cipherText, String keyBase64, int ivLength);
 }
