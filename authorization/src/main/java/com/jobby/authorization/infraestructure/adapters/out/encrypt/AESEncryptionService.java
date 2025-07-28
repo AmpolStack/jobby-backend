ad5c4de007c793453ab6f62e4ba289edb5233ad3
Amapola <htrsanbl@gmail.com>
2025-07-28 16:34:44 -0500

    feat(AESEncryptionService): Implements changes from interface and Utils style

--- authorization/src/main/java/com/jobby/authorization/infraestructure/adapters/out/encrypt/AESEncryptionService.java
+++ authorization/src/main/java/com/jobby/authorization/infraestructure/adapters/out/encrypt/AESEncryptionService.java
@@ -6,11 +6,11 @@ import com.jobby.authorization.domain.result.ErrorType;
 import com.jobby.authorization.domain.result.Field;
 import com.jobby.authorization.domain.result.Result;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.GCMParameterSpec;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -25,23 +25,12 @@ public class AESEncryptionService implements EncryptionService {
         this.encryptBuilder = defaultEncryptBuilder;
     }
 
-    @Override
-    public Result<String, Error> encrypt(String data, String keyBase64, int ivLength) {
-
+    private Result<Key, Error> validateAndParseKey(String keyBase64){
         if(keyBase64.isEmpty()) {
             return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
                     new Field(
                             "keyBase64",
                             "Encryption key cannot be empty"
-                    )
-            );
-        }
-
-        if(ivLength <= 0 || ivLength > 16){
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field(
-                            "ivLength",
-                            "IV length must be between 1 and 16 bytes"
                     )
             );
         }
@@ -57,7 +46,6 @@ public class AESEncryptionService implements EncryptionService {
             );
         }
 
-
         if (key.getEncoded().length < 16 || key.getEncoded().length > 32) {
             return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
                     new Field(
@@ -67,42 +55,45 @@ public class AESEncryptionService implements EncryptionService {
             );
         }
 
-        var iv = EncryptUtils.generateIv(key.getEncoded().length * 8, ivLength);
-
-        var executionResult = this.encryptBuilder
-                .setData(data.getBytes(StandardCharsets.UTF_8))
-                .setIv(iv)
-                .setKey(key)
-                .setMode(Cipher.ENCRYPT_MODE)
-                .setTransformation(TRANSFORMATION)
-                .build();
-
-        if(executionResult.isFailure()){
-            return Result.renewFailure(executionResult);
-        }
-
-        var cipherBytes = executionResult.getData();
-        var buffer = ByteBuffer.allocate(iv.getIV().length + cipherBytes.length);
-        buffer.put(iv.getIV());
-        buffer.put(cipherBytes);
-        var combined = buffer.array();
-
-        var response = Base64.getEncoder().encodeToString(combined);
-        return Result.success(response);
+        return Result.success(key);
     }
 
-    @Override
-    public Result<String, Error> decrypt(String cipherText, String keyBase64, int ivLength) {
-
-        if(keyBase64.isEmpty()) {
+    private Result<Void, Error> validateIvLength(int ivLength){
+        if(ivLength <= 0 || ivLength > 16){
             return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
                     new Field(
-                            "keyBase64",
-                            "Decryption key cannot be empty"
+                            "ivLength",
+                            "IV length must be between 1 and 16 bytes"
                     )
             );
         }
+        return Result.success(null);
+    }
 
+    @Override
+    public Result<String, Error> encrypt(String data, String keyBase64, int ivLength) {
+        return validateIvLength(ivLength)
+                .flatMap(x -> validateAndParseKey(keyBase64))
+                .flatMap((key -> {
+                    var iv = EncryptUtils.generateIv(key.getEncoded().length * 8, ivLength);
+                    return this.encryptBuilder
+                            .setData(data.getBytes(StandardCharsets.UTF_8))
+                            .setIv(iv)
+                            .setKey(key)
+                            .setMode(Cipher.ENCRYPT_MODE)
+                            .setTransformation(TRANSFORMATION)
+                            .build()
+                            .map(cipherBytes ->{
+                                var buffer = ByteBuffer.allocate(iv.getIV().length + cipherBytes.length);
+                                buffer.put(iv.getIV());
+                                buffer.put(cipherBytes);
+                                var combined = buffer.array();
+                                return Base64.getEncoder().encodeToString(combined);
+                            });
+                }));
+    }
+
+    private Result<byte[], Error> validateAndParseCipherText(String cipherText){
         if(cipherText.isEmpty()){
             return Result.failure(ErrorType.INVALID_INPUT,
                     new Field(
@@ -124,8 +115,11 @@ public class AESEncryptionService implements EncryptionService {
                     )
             );
         }
+        return Result.success(combined);
+    }
 
-        if(ivLength > combined.length){
+    private Result<Void, Error> validateCombinedAndIvLength(int ivLength, int combinedLength){
+        if(ivLength > combinedLength){
             return Result.failure(ErrorType.VALIDATION_ERROR,
                     new Field(
                             "cipherText",
@@ -133,45 +127,28 @@ public class AESEncryptionService implements EncryptionService {
                     )
             );
         }
+        return Result.success(null);
+    }
 
-        var key = EncryptUtils.ParseKeySpec(ALGORITHM, keyBase64);
+    @Override
+    public Result<String, Error> decrypt(String cipherText, String keyBase64, int ivLength) {
+        return validateAndParseCipherText(cipherText)
+                .flatMap(combined -> validateCombinedAndIvLength(ivLength, combined.length)
+                        .flatMap(n -> validateAndParseKey(keyBase64))
+                        .flatMap( key -> {
 
-        if(key == null){
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field(
-                            "keyBase64",
-                            "Invalid Base64 encoded key or incompatible key format"
-                    )
-            );
-        }
-
-        if (key.getEncoded().length < 16 || key.getEncoded().length > 32) {
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field(
-                            "keyBase64",
-                            "Key length must be between 16 and 32 bytes for AES decryption"
-                    )
-            );
-        }
-
-        var rawIv = Arrays.copyOfRange(combined, 0, ivLength);
-        var data = Arrays.copyOfRange(combined, ivLength, combined.length);
-
-        var iv = new GCMParameterSpec(key.getEncoded().length * 8, rawIv);
-
-        var executionResult = this.encryptBuilder
-                .setData(data)
-                .setIv(iv)
-                .setKey(key)
-                .setMode(Cipher.DECRYPT_MODE)
-                .setTransformation(TRANSFORMATION)
-                .build();
-
-        if(executionResult.isFailure()){
-            return Result.renewFailure(executionResult);
-        }
-
-        var resp = new String(executionResult.getData(), StandardCharsets.UTF_8);
-        return Result.success(resp);
+                            var rawIv = Arrays.copyOfRange(combined, 0, ivLength);
+                            var data = Arrays.copyOfRange(combined, ivLength, combined.length);
+                            var iv = new GCMParameterSpec(key.getEncoded().length * 8, rawIv);
+                            return this.encryptBuilder
+                                    .setData(data)
+                                    .setIv(iv)
+                                    .setKey(key)
+                                    .setMode(Cipher.DECRYPT_MODE)
+                                    .setTransformation(TRANSFORMATION)
+                                    .build()
+                                    .map(bytesResp -> new String(bytesResp, StandardCharsets.UTF_8));
+                        })
+                );
     }
 }
