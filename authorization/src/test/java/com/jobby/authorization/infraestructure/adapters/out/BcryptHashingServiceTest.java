89aac991e40e50abf45b898deb536d70b139a047
Amapola <htrsanbl@gmail.com>
2025-07-28 20:58:13 -0500

    test: update Bcrypt hashing service test

--- authorization/src/test/java/com/jobby/authorization/infraestructure/adapters/out/BcryptHashingServiceTest.java
+++ authorization/src/test/java/com/jobby/authorization/infraestructure/adapters/out/BcryptHashingServiceTest.java
@@ -1,11 +1,15 @@
 package com.jobby.authorization.infraestructure.adapters.out;
 
+import com.jobby.authorization.domain.result.Error;
+import com.jobby.authorization.domain.result.ErrorType;
+import com.jobby.authorization.domain.result.Field;
 import com.jobby.authorization.domain.result.Result;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.util.Base64;
+import java.util.Objects;
 import java.util.Random;
 
 public class BcryptHashingServiceTest {
@@ -56,9 +60,38 @@ public class BcryptHashingServiceTest {
     public void hash_WhenInputIsNull() {
         // Act
         var hashResult = this.bcryptHashingService.hash(null);
+        Result<String, Error> expectedResult = Result.failure(
+                ErrorType.VALIDATION_ERROR,
+                new Field(
+                        "input",
+                        "the input are null or blank"
+                )
+        );
 
         // Assert
         Assertions.assertFalse(hashResult.isSuccess());
+        Assertions.assertEquals(hashResult, expectedResult);
+    }
+
+    @Test
+    public void hash_WhenInputExceeds72Bytes() {
+        //Arrange
+        var bytes = new byte[73];
+        new Random().nextBytes(bytes);
+        var input = new String(bytes);
+        // Act
+        var hashResult = this.bcryptHashingService.hash(input);
+        Result<String, Error> expectedResult = Result.failure(
+                ErrorType.VALIDATION_ERROR,
+                new Field(
+                        "input",
+                        "the input are invalid, because exceeds the 72 bytes of length"
+                )
+        );
+
+        // Assert
+        Assertions.assertFalse(hashResult.isSuccess());
+        Assertions.assertEquals(hashResult, expectedResult);
     }
 
 
