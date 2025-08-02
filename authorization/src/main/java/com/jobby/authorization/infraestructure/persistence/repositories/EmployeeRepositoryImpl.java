8f286d007936549197a83c1cbb64271eb97b2636
Amapola <htrsanbl@gmail.com>
2025-08-02 01:12:03 -0500

    feat: Implements method in EmployeRepositoryImpl

--- authorization/src/main/java/com/jobby/authorization/infraestructure/persistence/repositories/EmployeeRepositoryImpl.java
+++ authorization/src/main/java/com/jobby/authorization/infraestructure/persistence/repositories/EmployeeRepositoryImpl.java
@@ -2,20 +2,26 @@ package com.jobby.authorization.infraestructure.persistence.repositories;
 
 import com.jobby.authorization.domain.model.Employee;
 import com.jobby.authorization.domain.ports.EmployeeRepository;
+import com.jobby.authorization.infraestructure.persistence.mappers.MongoEmployeeEntityMapper;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
 public class EmployeeRepositoryImpl implements EmployeeRepository {
 
+    private final MongoEmployeeEntityMapper mongoMapper;
     private final SpringDataMongoEmployeeRepository mongoRepository;
 
-    public EmployeeRepositoryImpl(SpringDataMongoEmployeeRepository mongoRepository) {
+    public EmployeeRepositoryImpl(MongoEmployeeEntityMapper mongoMapper, SpringDataMongoEmployeeRepository mongoRepository) {
+        this.mongoMapper = mongoMapper;
         this.mongoRepository = mongoRepository;
     }
 
     @Override
     public Optional<Employee> findByEmailAndPassword(String email, String password) {
-        return Optional.empty();
+        return this.mongoRepository
+                .findByEmailAndPassword(email, password)
+                .map(this.mongoMapper::toDomain);
     }
+
 }
