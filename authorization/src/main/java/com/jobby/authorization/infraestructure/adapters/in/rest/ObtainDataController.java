32b94b80eab6c463e50ca2f73d06db0c2a05a48c
Amapola <htrsanbl@gmail.com>
2025-08-02 15:31:49 -0500

    chore(pom.xml): Includes redis starter package

--- authorization/src/main/java/com/jobby/authorization/infraestructure/adapters/in/rest/ObtainDataController.java
+++ authorization/src/main/java/com/jobby/authorization/infraestructure/adapters/in/rest/ObtainDataController.java
@@ -5,8 +5,6 @@ import com.jobby.authorization.domain.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/obtain")
 public class ObtainDataController {
