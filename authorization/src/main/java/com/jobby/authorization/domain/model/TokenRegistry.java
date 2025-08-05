058f7c4a790bc9b0b87235ba70023fd3075946a8
Amapola <htrsanbl@gmail.com>
2025-08-05 17:12:03 -0500

    chore(TokenRegistry): Add constructor annotations

--- authorization/src/main/java/com/jobby/authorization/domain/model/TokenRegistry.java
+++ authorization/src/main/java/com/jobby/authorization/domain/model/TokenRegistry.java
@@ -1,12 +1,16 @@
 package com.jobby.authorization.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TokenRegistry {
     private int id;
     private String token;
