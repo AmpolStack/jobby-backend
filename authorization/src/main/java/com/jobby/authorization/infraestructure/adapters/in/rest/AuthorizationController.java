67c68c33769d7251374698b1d4b8b028fd338da1
Amapola <htrsanbl@gmail.com>
2025-08-09 18:49:10 -0500

    feat(UseCase): rename authorizeUseCase

---
+++ authorization/src/main/java/com/jobby/authorization/infraestructure/adapters/in/rest/AuthorizationController.java
@@ -0,0 +1,37 @@
+package com.jobby.authorization.infraestructure.adapters.in.rest;
+
+import com.jobby.authorization.domain.ports.in.AuthorizeEmployeeWithCredentialsUseCase;
+import com.jobby.authorization.domain.ports.out.SafeResultValidator;
+import com.jobby.authorization.domain.result.Error;
+import com.jobby.authorization.domain.result.Result;
+import com.jobby.authorization.infraestructure.dto.mappers.TokenRegistryResponseMapper;
+import com.jobby.authorization.infraestructure.dto.requests.LoginRequest;
+import com.jobby.authorization.infraestructure.dto.responses.TokenRegistryResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authorize")
+public class AuthController {
+
+    private final SafeResultValidator validator;
+    private final TokenRegistryResponseMapper responseMapper;
+    private final AuthorizeEmployeeWithCredentialsUseCase authorizeEmployeeWithCredentialsUseCase;
+
+    public AuthController(SafeResultValidator validator, TokenRegistryResponseMapper responseMapper, AuthorizeEmployeeWithCredentialsUseCase authorizeEmployeeWithCredentialsUseCase) {
+        this.validator = validator;
+        this.responseMapper = responseMapper;
+        this.authorizeEmployeeWithCredentialsUseCase = authorizeEmployeeWithCredentialsUseCase;
+    }
+
+    @PostMapping("/withCredentials")
+    public Result<TokenRegistryResponse, Error> withCredentials(@RequestBody LoginRequest request) {
+        return this.validator.validate(request)
+                .flatMap(x -> this.authorizeEmployeeWithCredentialsUseCase.byCredentials(request.getEmail(), request.getPassword()))
+                .map(this.responseMapper::toDto);
+    }
+
+
+}
