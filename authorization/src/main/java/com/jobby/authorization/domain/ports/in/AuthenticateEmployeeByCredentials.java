c96ae6f00bffe182e082b75fde915ca993456914
Amapola <htrsanbl@gmail.com>
2025-08-10 18:07:24 -0500

    chore(domain.ports.in): rename files

--- authorization/src/main/java/com/jobby/authorization/domain/ports/in/AuthenticateEmployeeByCredentials.java
+++ authorization/src/main/java/com/jobby/authorization/domain/ports/in/AuthenticateEmployeeByCredentials.java
@@ -3,6 +3,6 @@ package com.jobby.authorization.domain.ports.in;
 import com.jobby.authorization.domain.shared.result.Error;
 import com.jobby.authorization.domain.shared.result.Result;
 
-public interface AuthenticateEmployeeUseCase {
+public interface AuthenticateEmployeeWithCredentialsUseCase {
     Result<Void, Error> authenticate();
 }
