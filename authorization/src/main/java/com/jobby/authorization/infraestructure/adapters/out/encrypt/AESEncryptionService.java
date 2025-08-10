38f41350fd8a8bae864b369bd44f3d96fb64beaf
Amapola <htrsanbl@gmail.com>
2025-08-10 14:15:49 -0500

    refactor(AESEncryptionService): the functionalities are refactored with the use of Validators

--- authorization/src/main/java/com/jobby/authorization/infraestructure/adapters/out/encrypt/AESEncryptionService.java
+++ authorization/src/main/java/com/jobby/authorization/infraestructure/adapters/out/encrypt/AESEncryptionService.java
@@ -1,6 +1,10 @@
 package com.jobby.authorization.infraestructure.adapters.out.encrypt;
 
+import com.jobby.authorization.domain.ports.out.SafeResultValidator;
 import com.jobby.authorization.domain.ports.out.encrypt.EncryptionService;
+import com.jobby.authorization.domain.shared.validators.NumberValidator;
+import com.jobby.authorization.domain.shared.validators.ObjectValidator;
+import com.jobby.authorization.domain.shared.validators.StringValidator;
 import com.jobby.authorization.domain.shared.result.Error;
 import com.jobby.authorization.domain.shared.result.ErrorType;
 import com.jobby.authorization.domain.shared.result.Field;
@@ -14,93 +18,51 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Objects;
 
 @Service
 public class AESEncryptionService implements EncryptionService {
     private static final String ALGORITHM = "AES";
     private static final String TRANSFORMATION = "AES/GCM/NoPadding";
-    private static final int[] VALID_T_LENGTHS_BITS = new int[] {98, 112, 120, 128};
-    private static final int[] VALID_KEY_LENGTHS_BITS = new int[] {128, 192, 256};
+    private static final Integer[] VALID_T_LENGTHS_BITS = {98, 112, 120, 128};
+    private static final Integer[] VALID_KEY_LENGTHS_BITS = {128, 192, 256};
 
+    private final SafeResultValidator validator;
     private final DefaultEncryptBuilder encryptBuilder;
 
-    public AESEncryptionService(DefaultEncryptBuilder defaultEncryptBuilder) {
+    public AESEncryptionService(SafeResultValidator validator, DefaultEncryptBuilder defaultEncryptBuilder) {
+        this.validator = validator;
         this.encryptBuilder = defaultEncryptBuilder;
     }
 
-    private Result<Key, Error> validateAndParseKey(String keyBase64){
-        if(keyBase64 == null || keyBase64.isBlank()) {
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field(
-                            "keyBase64",
-                            "Encryption key cannot be empty"
-                    )
-            );
-        }
-
-        var key = EncryptUtils.ParseKeySpec(ALGORITHM, keyBase64);
-
-        if(key == null){
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field(
-                            "keyBase64",
-                            "Invalid Base64 encoded key or incompatible key format"
-                    )
-            );
-        }
-
-        var keyLengthInBytes = key.getEncoded().length * 8;
-        if (Arrays.stream(VALID_KEY_LENGTHS_BITS).noneMatch(length -> length == keyLengthInBytes)) {
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field(
-                            "keyBase64",
-                            "Key length must be between 16 and 32 bytes for AES encryption"
-                    )
-            );
-        }
-
-        return Result.success(key);
-    }
-
-    private Result<Void, Error> validateIvLength(int ivLength){
-        if(ivLength <= 0 || ivLength > 16){
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field(
-                            "ivLength",
-                            "IV length must be between 1 and 16 bytes"
-                    )
-            );
-        }
-        return Result.success(null);
-    }
-
-    private Result<Void, Error> validateTLen(int tLen){
-        if(Arrays.stream(VALID_T_LENGTHS_BITS).noneMatch(length -> length == tLen)){
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field("tLen", "TLen are invalid"));
-        }
-        return Result.success(null);
-    }
-
-    private Result<Void, Error> validateConfig(EncryptConfig encryptConfig){
-        if(encryptConfig == null){
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field("encryptConfig", "Encryption config cannot be null")
-            );
-        }
-        if(encryptConfig.getIv() == null){
-            return Result.failure(ErrorType.ITN_INVALID_OPTION_PARAMETER,
-                    new Field("encryptConfig", "Iv cannot be null")
-            );
-        }
-        return Result.success(null);
+    @Override
+    public Result<String, Error> decrypt(String cipherText, EncryptConfig config) {
+        return  this.validator.validate(config)
+                .flatMap(v -> ObjectValidator.validateAnyMatch(config.getIv().getTLen(), VALID_T_LENGTHS_BITS, "tLen"))
+                .flatMap(v -> validateAndParseCipherText(cipherText))
+                .flatMap(combined -> NumberValidator.validateGreaterInteger(config.getIv().getLength(), combined.length, "iv-length")
+                        .flatMap(v -> validateAndParseKey(config.getSecretKey()))
+                        .flatMap( key -> {
+                            var rawIv = Arrays.copyOfRange(combined, 0, config.getIv().getLength());
+                            var data = Arrays.copyOfRange(combined, config.getIv().getLength(), combined.length);
+                            var iv = new GCMParameterSpec(config.getIv().getTLen(), rawIv);
+                            return this.encryptBuilder
+                                    .setData(data)
+                                    .setIv(iv)
+                                    .setKey(key)
+                                    .setMode(Cipher.DECRYPT_MODE)
+                                    .setTransformation(TRANSFORMATION)
+                                    .build()
+                                    .map(bytesResp -> new String(bytesResp, StandardCharsets.UTF_8));
+                        })
+                );
     }
 
     @Override
     public Result<String, Error> encrypt(String data, EncryptConfig config) {
-        return validateConfig(config)
+        return this.validator.validate(config)
                 .flatMap(x -> validateIvLength(config.getIv().getLength()))
-                .flatMap(x -> validateTLen(config.getIv().getTLen()))
+                .flatMap(x -> ObjectValidator.validateAnyMatch(config.getIv().getTLen(), VALID_T_LENGTHS_BITS, "tLen"))
                 .flatMap(x -> validateAndParseKey(config.getSecretKey()))
                 .flatMap((key -> {
                     var iv = EncryptUtils.generateIv(config.getIv().getLength(), config.getIv().getTLen());
@@ -121,64 +83,47 @@ public class AESEncryptionService implements EncryptionService {
                 }));
     }
 
+    private Result<Key, Error> validateAndParseKey(String keyBase64){
+        final int BIT_MULTIPLIER = 8;
+        return StringValidator.validateNotBlankString(keyBase64, "keyBase64")
+                .flatMap(v -> {
+                    var key = EncryptUtils.ParseKeySpec(ALGORITHM, keyBase64);
+                    return ObjectValidator.validateNotNullObject(key, "parsedKey")
+                            .flatMap(v2 -> {
+                                var keyLengthInBytes = Objects.requireNonNull(key).getEncoded().length * BIT_MULTIPLIER;
+                                return ObjectValidator.validateAnyMatch(keyLengthInBytes, VALID_KEY_LENGTHS_BITS, "keyBase64-bytes")
+                                        .map(v3 -> key);
+                            });
+
+                });
+    }
+
+    private Result<Void, Error> validateIvLength(int ivLength){
+        final var name = "ivLength";
+        final int IV_LENGTH_LOWER_LIMIT = 0;
+        final int IV_LENGTH_UPPER_LIMIT = 16;
+
+        return NumberValidator.validateGreaterNotEqualsInteger(ivLength, IV_LENGTH_LOWER_LIMIT, name)
+                .flatMap(v -> NumberValidator.validateSmallerInteger(ivLength, IV_LENGTH_UPPER_LIMIT, name));
+    }
+
     private Result<byte[], Error> validateAndParseCipherText(String cipherText){
-        if(cipherText == null ||  cipherText.isBlank()){
-            return Result.failure(ErrorType.INVALID_INPUT,
-                    new Field(
-                            "cipherText",
-                            "Cipher text cannot be null or blank"
-                    )
-            );
-        }
-
-        byte[] combined;
-        try{
-            combined = Base64.getDecoder().decode(cipherText);
-        }
-        catch (IllegalArgumentException e){
-            return Result.failure(ErrorType.INVALID_INPUT,
-                    new Field(
-                            "cipherText",
-                            "Invalid Base64 encoded cipher text"
-                    )
-            );
-        }
-        return Result.success(combined);
+        return StringValidator.validateNotBlankString(cipherText, "cipher-text")
+                .flatMap(v -> {
+                    byte[] combined;
+                    try{
+                        combined = Base64.getDecoder().decode(cipherText);
+                    }
+                    catch (IllegalArgumentException e){
+                        return Result.failure(ErrorType.INVALID_INPUT,
+                                new Field(
+                                        "cipherText",
+                                        "Invalid Base64 encoded cipher text"
+                                )
+                        );
+                    }
+                    return Result.success(combined);
+                });
     }
 
-    private Result<Void, Error> validateCombinedAndIvLength(int ivLength, int combinedLength){
-        if(ivLength > combinedLength){
-            return Result.failure(ErrorType.VALIDATION_ERROR,
-                    new Field(
-                            "cipherText",
-                            "Cipher text is too short to contain valid data (expected at least " + ivLength + " bytes)"
-                    )
-            );
-        }
-        return Result.success(null);
-    }
-
-    @Override
-    public Result<String, Error> decrypt(String cipherText, EncryptConfig config) {
-        return  validateConfig(config)
-                .flatMap(x -> validateTLen(config.getIv().getTLen()))
-                .flatMap(x -> validateAndParseCipherText(cipherText))
-                .flatMap(combined -> validateCombinedAndIvLength(config.getIv().getLength(), combined.length)
-                        .flatMap(n -> validateAndParseKey(config.getSecretKey()))
-                        .flatMap( key -> {
-
-                            var rawIv = Arrays.copyOfRange(combined, 0, config.getIv().getLength());
-                            var data = Arrays.copyOfRange(combined, config.getIv().getLength(), combined.length);
-                            var iv = new GCMParameterSpec(config.getIv().getTLen(), rawIv);
-                            return this.encryptBuilder
-                                    .setData(data)
-                                    .setIv(iv)
-                                    .setKey(key)
-                                    .setMode(Cipher.DECRYPT_MODE)
-                                    .setTransformation(TRANSFORMATION)
-                                    .build()
-                                    .map(bytesResp -> new String(bytesResp, StandardCharsets.UTF_8));
-                        })
-                );
-    }
 }
