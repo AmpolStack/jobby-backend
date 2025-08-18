e054a9f3c1ccbeb549eec5fcf32e2a838cbf8c3e
Amapola <htrsanbl@gmail.com>
2025-08-18 16:34:24 -0500

    feat(Injections): add the manual bean injection

--- authorization/src/main/java/com/jobby/authorization/infraestructure/config/Injections.java
+++ authorization/src/main/java/com/jobby/authorization/infraestructure/config/Injections.java
@@ -1,5 +1,7 @@
 package com.jobby.authorization.infraestructure.config;
 
+import com.jobby.authorization.infraestructure.response.definition.ApiResponseMapper;
+import com.jobby.authorization.infraestructure.response.implementation.problem.ProblemDetailsResultMapper;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +18,10 @@ public class Injections {
     @ConfigurationProperties(prefix = "token")
     public TokenConfig getTokenConfig(){
         return new TokenConfig();
+    }
+
+    @Bean
+    public ApiResponseMapper getRestResponseMapper(){
+        return new ProblemDetailsResultMapper();
     }
 }
