176aff8fd469e41bd078f2bfbe0df729543e6936
Amapola <htrsanbl@gmail.com>
2025-08-19 17:56:14 -0500

    chore: upgrade entity

--- employee/src/main/java/com/jobby/employee/domain/model/Employee.java
+++ employee/src/main/java/com/jobby/employee/domain/model/Employee.java
@@ -3,12 +3,14 @@ package com.jobby.employee.domain.model;
 import java.util.Date;
 
 public class Employee {
-    private int employeeId;
+    private int id;
     private Address address;
     private User user;
+    private String username;
     private String password;
     private String positionName;
     private String profileImageUrl;
     private Date createdAt;
+    private String status;
     private Date modifiedAt;
 }
