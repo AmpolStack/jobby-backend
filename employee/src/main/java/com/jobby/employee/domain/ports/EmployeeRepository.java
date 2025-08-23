84868f995f824688b4ecb0f43581b47ec82536b9
Amapola <htrsanbl@gmail.com>
2025-08-23 12:56:19 -0500

    feat(employeeRepository): The domain interface of employee are included

---
+++ employee/src/main/java/com/jobby/employee/domain/ports/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.jobby.employee.domain.ports;
+
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
+import com.jobby.employee.domain.model.Employee;
+
+public interface EmployeeRepository {
+    Result<Employee, Error> save(Employee employee);
+}
