7b04e873988f3078314f4c707d354348c3b285ba
Amapola <htrsanbl@gmail.com>
2025-08-23 22:41:15 -0500

    feat: add first mapper

--- employee/src/main/java/com/jobby/employee/infraestructure/persistence/mappers/JpaEmployeeMapper.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/persistence/mappers/JpaEmployeeMapper.java
@@ -5,19 +5,10 @@ import com.jobby.employee.infraestructure.persistence.entities.JpaEmployeeEntity
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { JpaAddressMapper.class, JpaSectionalMapper.class })
 public interface JpaEmployeeMapper {
-    
-    @Mapping(target = "address", ignore = true)
+
     @Mapping(target = "user", ignore = true)
-    @Mapping(target = "sectional", ignore = true)
     @Mapping(target = "status", ignore = true)
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "username", source = "username")
-    @Mapping(target = "password", source = "password")
-    @Mapping(target = "positionName", source = "positionName")
-    @Mapping(target = "profileImageUrl", source = "profileImageUrl")
-    @Mapping(target = "createdAt", source = "createdAt")
-    @Mapping(target = "modifiedAt", source = "modifiedAt")
     Employee toDomain(JpaEmployeeEntity jpaEmployeeEntity);
 }
