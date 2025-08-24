ae01249b3c5e8e7e7b99e9a5b27ba02042f44eeb
Amapola <htrsanbl@gmail.com>
2025-08-23 23:59:26 -0500

    feat(EmployeeController): Implements the problem details mapper with api response component

--- employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/EmployeeController.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.jobby.employee.infraestructure.adapters.in;
 
 import com.jobby.employee.domain.ports.in.GetEmployeeByIdUseCase;
+import com.jobby.infraestructure.response.definition.ApiResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private final GetEmployeeByIdUseCase getEmployeeByIdUseCase;
+    private final ApiResponseMapper apiResponseMapper;
 
-    public EmployeeController(GetEmployeeByIdUseCase getEmployeeByIdUseCase) {
+    public EmployeeController(GetEmployeeByIdUseCase getEmployeeByIdUseCase, ApiResponseMapper apiResponseMapper) {
         this.getEmployeeByIdUseCase = getEmployeeByIdUseCase;
+        this.apiResponseMapper = apiResponseMapper;
     }
 
     @GetMapping("/findById")
     public ResponseEntity<?> getById(@RequestParam int id) {
         var resp = this.getEmployeeByIdUseCase
                 .execute(id);
-        return ResponseEntity.ok(resp);
+
+        return this.apiResponseMapper.map(resp);
     }
 }
