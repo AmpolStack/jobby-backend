05cd7a0ccee35b69819444fc841f90a6ed0c4dda
Amapola <htrsanbl@gmail.com>
2025-08-23 23:40:20 -0500

    feat: The UseCase for getById are included, and delegates the validation in use case for fast response

---
+++ employee/src/main/java/com/jobby/employee/application/useCases/DefaultGetEmployeeByIdUseCase.java
@@ -0,0 +1,28 @@
+package com.jobby.employee.application.useCases;
+
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
+import com.jobby.domain.mobility.validator.ValidationChain;
+import com.jobby.employee.domain.model.Employee;
+import com.jobby.employee.domain.ports.in.GetEmployeeByIdUseCase;
+import com.jobby.employee.domain.ports.out.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultGetEmployeeByIdUseCase implements GetEmployeeByIdUseCase {
+
+    private final EmployeeRepository employeeRepository;
+
+    public DefaultGetEmployeeByIdUseCase(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Result<Employee, Error> execute(int id) {
+        return ValidationChain
+                .create()
+                .validateGreaterOrEqualsThan(id, 0, "employee-id")
+                .build()
+                .flatMap(v -> this.employeeRepository.getEmployeeById(id));
+    }
+}
--- employee/src/main/java/com/jobby/employee/infraestructure/adapters/out/DefaultEmployeeRepository.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/adapters/out/DefaultEmployeeRepository.java
@@ -4,9 +4,8 @@ import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.error.ErrorType;
 import com.jobby.domain.mobility.error.Field;
 import com.jobby.domain.mobility.result.Result;
-import com.jobby.domain.mobility.validator.ValidationChain;
 import com.jobby.employee.domain.model.Employee;
-import com.jobby.employee.domain.ports.EmployeeRepository;
+import com.jobby.employee.domain.ports.out.EmployeeRepository;
 import com.jobby.employee.infraestructure.persistence.mappers.JpaEmployeeMapper;
 import com.jobby.employee.infraestructure.persistence.repositories.SpringDataJpaEmployeeRepository;
 import org.springframework.stereotype.Component;
@@ -28,18 +27,13 @@ public class DefaultEmployeeRepository implements EmployeeRepository {
 
     @Override
     public Result<Employee, Error> getEmployeeById(int id) {
-        return ValidationChain.create()
-                .validateGreaterOrEqualsThan(id, 0, "employee-id")
-                .build()
-                .flatMap(v ->{
-                        var res = this.employeeRepository.findById(id);
-                        var comp = res.map(this.jpaEmployeeMapper::toDomain);
-                        return comp.map(Result::<Employee, Error>success)
-                        .orElseGet(()-> Result.failure(
-                                ErrorType.USER_NOT_FOUND,
-                                new Field("employee", "No employee found with given id")
-                        ));
-                }
-                );
+        return this.employeeRepository
+                .findById(id)
+                .map(this.jpaEmployeeMapper::toDomain)
+                .map(Result::<Employee, Error>success)
+                .orElseGet(()-> Result.failure(
+                        ErrorType.USER_NOT_FOUND,
+                        new Field("employee", "No employee found with given id")
+                ));
     }
 }
