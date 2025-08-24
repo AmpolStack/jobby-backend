6578d6569c66c28fe319ddc23c11e2a5160057d7
Amapola <htrsanbl@gmail.com>
2025-08-23 21:26:06 -0500

    refactor: the repository are included

--- employee/src/main/java/com/jobby/employee/infraestructure/adapters/out/DefaultEmployeeRepository.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/adapters/out/DefaultEmployeeRepository.java
@@ -1,7 +1,10 @@
 package com.jobby.employee.infraestructure.adapters.out;
 
 import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.error.ErrorType;
+import com.jobby.domain.mobility.error.Field;
 import com.jobby.domain.mobility.result.Result;
+import com.jobby.domain.mobility.validator.ValidationChain;
 import com.jobby.employee.domain.model.Employee;
 import com.jobby.employee.domain.ports.EmployeeRepository;
 import com.jobby.employee.infraestructure.persistence.mappers.JpaEmployeeMapper;
@@ -21,5 +24,22 @@ public class DefaultEmployeeRepository implements EmployeeRepository {
     @Override
     public Result<Employee, Error> save(Employee employee) {
         return null;
+    }
+
+    @Override
+    public Result<Employee, Error> getEmployeeById(int id) {
+        return ValidationChain.create()
+                .validateGreaterOrEqualsThan(id, 0, "employee-id")
+                .build()
+                .flatMap(v ->{
+                        var res = this.employeeRepository.findById(id);
+                        var comp = res.map(this.jpaEmployeeMapper::toDomain);
+                        return comp.map(Result::<Employee, Error>success)
+                        .orElseGet(()-> Result.failure(
+                                ErrorType.USER_NOT_FOUND,
+                                new Field("employee", "No employee found with given id")
+                        ));
+                }
+                );
     }
 }
