cc0d002d47ccb737109ae796b55f589be1174fb7
Amapola <htrsanbl@gmail.com>
2025-08-23 21:23:39 -0500

    feat(Employee): Properties and Types are upgraded and added

--- employee/src/main/java/com/jobby/employee/domain/model/Employee.java
+++ employee/src/main/java/com/jobby/employee/domain/model/Employee.java
@@ -1,16 +1,21 @@
 package com.jobby.employee.domain.model;
 
-import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.Instant;
 
+@Getter
+@Setter
 public class Employee {
     private int id;
     private Address address;
     private User user;
+    private Sectional sectional;
+    private EmployeeStatus status;
     private String username;
     private String password;
     private String positionName;
     private String profileImageUrl;
-    private Date createdAt;
-    private String status;
-    private Date modifiedAt;
+    private Instant createdAt;
+    private Instant modifiedAt;
 }
