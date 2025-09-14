fcd4e88f93b94fe7cf2bf683c2a43d681c2b8a47
Amapola <htrsanbl@gmail.com>
2025-09-14 17:24:50 -0500

    chore: Includes the bean autoconfiguration for the services

---
+++ shared/src/main/java/com/jobby/infraestructure/autoconfiguration/EncryptionAutoConfiguration.java
@@ -0,0 +1,31 @@
+package com.jobby.infraestructure.autoconfiguration;
+
+import com.jobby.domain.ports.SafeResultValidator;
+import com.jobby.domain.ports.encrypt.EncryptBuilder;
+import com.jobby.domain.ports.encrypt.EncryptionService;
+import com.jobby.infraestructure.adapter.encrypt.AESEncryptionService;
+import com.jobby.infraestructure.adapter.encrypt.DefaultEncryptBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({EncryptBuilder.class})
+public class EncryptionAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EncryptBuilder encryptBuilder() {
+        return new DefaultEncryptBuilder();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EncryptionService encryptionService(
+            SafeResultValidator safeResultValidator,
+            EncryptBuilder encryptBuilder) {
+        return new AESEncryptionService(safeResultValidator, encryptBuilder);
+    }
+
+}
