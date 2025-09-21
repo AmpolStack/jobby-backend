89a861746603199e13f99495927f368334b6921e
Amapola <htrsanbl@gmail.com>
2025-09-21 00:16:36 -0500

    feat: Add endpoint for post employee

--- employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/EmployeeController.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.jobby.employee.infraestructure.adapters.in;
 
 import com.jobby.employee.domain.ports.in.GetEmployeeByIdUseCase;
+import com.jobby.employee.domain.ports.in.PutEmployeeUseCase;
+import com.jobby.employee.infraestructure.dto.EmployeeCreated;
+import com.jobby.employee.infraestructure.mappers.EmployeeCreatedMapper;
 import com.jobby.infraestructure.response.definition.ApiResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +13,30 @@ import org.springframework.web.bind.annotation.*;
 public class EmployeeController {
 
     private final GetEmployeeByIdUseCase getEmployeeByIdUseCase;
+    private final PutEmployeeUseCase putEmployeeUseCase;
     private final ApiResponseMapper apiResponseMapper;
+    private final EmployeeCreatedMapper employeeCreatedMapper;
 
-    public EmployeeController(GetEmployeeByIdUseCase getEmployeeByIdUseCase, ApiResponseMapper apiResponseMapper) {
+    public EmployeeController(GetEmployeeByIdUseCase getEmployeeByIdUseCase, PutEmployeeUseCase putEmployeeUseCase, ApiResponseMapper apiResponseMapper, EmployeeCreatedMapper employeeCreatedMapper) {
         this.getEmployeeByIdUseCase = getEmployeeByIdUseCase;
+        this.putEmployeeUseCase = putEmployeeUseCase;
         this.apiResponseMapper = apiResponseMapper;
+        this.employeeCreatedMapper = employeeCreatedMapper;
     }
 
     @GetMapping("/findById")
     public ResponseEntity<?> getById(@RequestHeader("X-User-Id") int id) {
         var resp = this.getEmployeeByIdUseCase
                 .execute(id);
+
+        return this.apiResponseMapper.map(resp);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody EmployeeCreated employee) {
+        var domainEmployee = this.employeeCreatedMapper.toDomain(employee);
+        var resp = this.putEmployeeUseCase
+                .execute(domainEmployee);
 
         return this.apiResponseMapper.map(resp);
     }
