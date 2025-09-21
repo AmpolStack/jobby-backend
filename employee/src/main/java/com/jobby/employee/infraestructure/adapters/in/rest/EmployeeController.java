b313da529867325d2c076607647d9a3bedd9fad9
Amapola <htrsanbl@gmail.com>
2025-09-21 00:54:29 -0500

    refactor: changes in dtos path

--- employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/rest/EmployeeController.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/rest/EmployeeController.java
@@ -2,8 +2,8 @@ package com.jobby.employee.infraestructure.adapters.in;
 
 import com.jobby.employee.domain.ports.in.GetEmployeeByIdUseCase;
 import com.jobby.employee.domain.ports.in.PutEmployeeUseCase;
-import com.jobby.employee.infraestructure.dto.EmployeeCreated;
-import com.jobby.employee.infraestructure.mappers.EmployeeCreatedMapper;
+import com.jobby.employee.infraestructure.dtos.objects.created.EmployeeCreated;
+import com.jobby.employee.infraestructure.dtos.mappers.EmployeeCreatedMapper;
 import com.jobby.infraestructure.response.definition.ApiResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- employee/src/main/java/com/jobby/employee/infraestructure/dtos/objects/created/EmployeeCreated.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/dtos/objects/created/EmployeeCreated.java
@@ -1,4 +1,4 @@
-package com.jobby.employee.infraestructure.dto;
+package com.jobby.employee.infraestructure.dtos.objects.created;
 
 import lombok.Getter;
 import lombok.Setter;
