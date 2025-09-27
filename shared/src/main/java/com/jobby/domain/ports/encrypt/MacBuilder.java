da6d45fdec7f2aeda2b73e5b638643477aef0ee7
Amapola <htrsanbl@gmail.com>
2025-09-26 21:33:41 -0500

    chore(MacBuilder): The builder service is included

---
+++ shared/src/main/java/com/jobby/domain/ports/encrypt/MacBuilder.java
@@ -0,0 +1,13 @@
+package com.jobby.domain.ports.encrypt;
+
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
+import com.jobby.infraestructure.adapter.encrypt.DefaultMacBuilder;
+import java.security.Key;
+
+public interface MacBuilder {
+    DefaultMacBuilder setData(byte[] data);
+    DefaultMacBuilder setKey(Key key);
+    DefaultMacBuilder setAlgorithm(String algorithm);
+    Result<byte[], Error> build();
+}
