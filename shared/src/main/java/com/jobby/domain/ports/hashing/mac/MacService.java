6c04ada88e016be6a37522ed0cab907edbbd1e05
Amapola <htrsanbl@gmail.com>
2025-09-30 23:37:03 -0500

    refactor(MacService): Now returns and use byte array

--- shared/src/main/java/com/jobby/domain/ports/hashing/mac/MacService.java
+++ shared/src/main/java/com/jobby/domain/ports/hashing/mac/MacService.java
@@ -5,6 +5,6 @@ import com.jobby.domain.mobility.error.Error;
 import com.jobby.domain.mobility.result.Result;
 
 public interface MacService {
-    Result<String, Error> generateMac(String data, MacConfig config);
-    Result<Boolean, Error> verifyMac(String data, String mac, MacConfig config);
+    Result<byte[], Error> generateMac(String data, MacConfig config);
+    Result<Boolean, Error> verifyMac(String data, byte[] mac, MacConfig config);
 }
