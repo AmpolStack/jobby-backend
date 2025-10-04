23d6aa2b77fe23d54dce45b28b0a6f04eb8f542b
Amapola <htrsanbl@gmail.com>
2025-10-04 18:47:44 -0500

    feat(business.mongo.entities): The mongo entities are included

---
+++ business/src/main/java/com/jobby/business/infrastructure/persistence/mongo/entities/MongoAddressEntity.java
@@ -0,0 +1,25 @@
+package com.jobby.business.infrastructure.persistence.mongo.entities;
+
+import com.jobby.infraestructure.common.MacGeneratedProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.bind.Name;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class MongoAddressEntity {
+    @Name("address_id")
+    private int id;
+    private MongoCityEntity city;
+    private String value;
+    @Name("value_searchable")
+    @MacGeneratedProperty(name = "value")
+    private byte[] valueSearchable;
+    private String description;
+    @Name("created_at")
+    private Date createdAt;
+    @Name("modified_at")
+    private Date modifiedAt;
+}
---
+++ business/src/main/java/com/jobby/business/infrastructure/persistence/mongo/entities/MongoBusinessEntity.java
@@ -0,0 +1,27 @@
+package com.jobby.business.infrastructure.persistence.mongo.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.bind.Name;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@Document("business_info")
+public class MongoBusinessEntity {
+    @Name("business_id")
+    private int id;
+    private MongoAddressEntity address;
+    private String name;
+    @Name("banner_image_url")
+    private String bannerImageUrl;
+    @Name("profile_image_url")
+    private String profileImageUrl;
+    private String description;
+    @Name("created_at")
+    private Instant createdAt;
+    @Name("modified_at")
+    private Instant modifiedAt;
+}
