8d20403e2e5df693943091442d6e63ac02e14ef4
Amapola <htrsanbl@gmail.com>
2025-10-03 22:19:21 -0500

    chore(EncryptionAutoConfiguration): The encryption config are included

--- shared/src/main/java/com/jobby/infraestructure/autoconfiguration/EncryptionAutoConfiguration.java
+++ shared/src/main/java/com/jobby/infraestructure/autoconfiguration/EncryptionAutoConfiguration.java
@@ -1,10 +1,12 @@
 package com.jobby.infraestructure.autoconfiguration;
 
+import com.jobby.domain.configurations.EncryptConfig;
 import com.jobby.domain.ports.SafeResultValidator;
 import com.jobby.domain.ports.encrypt.EncryptBuilder;
 import com.jobby.domain.ports.encrypt.EncryptionService;
 import com.jobby.infraestructure.adapter.encrypt.AESEncryptionService;
 import com.jobby.infraestructure.adapter.encrypt.DefaultEncryptBuilder;
+import com.jobby.infraestructure.common.EncryptionPropertyInitializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -27,5 +29,16 @@ public class EncryptionAutoConfiguration {
             EncryptBuilder encryptBuilder) {
         return new AESEncryptionService(safeResultValidator, encryptBuilder);
     }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EncryptionPropertyInitializer encryptionPropertyInitializer(
+            EncryptConfig encryptConfig,
+            EncryptionService encryptionService,
+            SafeResultValidator safeResultValidator
+    ){
+        return new EncryptionPropertyInitializer(encryptConfig, encryptionService, safeResultValidator);
+    }
+
 
 }
