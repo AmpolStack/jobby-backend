356e220da22048a38368c6e1138643a895568000
Amapola <htrsanbl@gmail.com>
2025-10-03 22:26:16 -0500

    refactor: update controller

--- employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/rest/controllers/EmployeeController.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/adapters/in/rest/controllers/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.jobby.employee.infraestructure.adapters.in.rest.controllers;
 
+import com.jobby.domain.ports.encrypt.EncryptionService;
+import com.jobby.employee.domain.model.Employee;
 import com.jobby.employee.domain.ports.in.GetEmployeeByIdUseCase;
 import com.jobby.employee.domain.ports.in.PutEmployeeUseCase;
 import com.jobby.employee.infraestructure.adapters.in.rest.dto.created.EmployeeCreated;
 import com.jobby.employee.infraestructure.adapters.in.rest.mappers.EmployeeCreatedMapper;
+import com.jobby.employee.infraestructure.persistence.jpa.entities.JpaAppUserEntity;
+import com.jobby.employee.infraestructure.persistence.jpa.entities.JpaEmployeeEntity;
+import com.jobby.infraestructure.common.EncryptionPropertyInitializer;
+import com.jobby.infraestructure.common.MacPropertyInitializer;
 import com.jobby.infraestructure.response.definition.ApiResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +22,16 @@ public class EmployeeController {
     private final PutEmployeeUseCase putEmployeeUseCase;
     private final ApiResponseMapper apiResponseMapper;
     private final EmployeeCreatedMapper employeeCreatedMapper;
+    private final MacPropertyInitializer macPropertyInitializer;
+    private final EncryptionPropertyInitializer encryptionPropertyInitializer;
 
-    public EmployeeController(GetEmployeeByIdUseCase getEmployeeByIdUseCase, PutEmployeeUseCase putEmployeeUseCase, ApiResponseMapper apiResponseMapper, EmployeeCreatedMapper employeeCreatedMapper) {
+    public EmployeeController(GetEmployeeByIdUseCase getEmployeeByIdUseCase, PutEmployeeUseCase putEmployeeUseCase, ApiResponseMapper apiResponseMapper, EmployeeCreatedMapper employeeCreatedMapper, MacPropertyInitializer macPropertyInitializer, EncryptionPropertyInitializer encryptionPropertyInitializer) {
         this.getEmployeeByIdUseCase = getEmployeeByIdUseCase;
         this.putEmployeeUseCase = putEmployeeUseCase;
         this.apiResponseMapper = apiResponseMapper;
         this.employeeCreatedMapper = employeeCreatedMapper;
+        this.macPropertyInitializer = macPropertyInitializer;
+        this.encryptionPropertyInitializer = encryptionPropertyInitializer;
     }
 
     @GetMapping("/findById")
@@ -39,5 +49,32 @@ public class EmployeeController {
                 .execute(domainEmployee);
 
         return this.apiResponseMapper.map(resp);
+    }
+
+    @PostMapping("/lamp")
+    public ResponseEntity<?> lamp() {
+        var com = new JpaAppUserEntity();
+        com.setEmail("comp");
+        com.setFirstName("first");
+        com.setLastName("last");
+        com.setPhone("32421");
+
+        var employee = new JpaEmployeeEntity();
+        employee.setUsername("employee");
+        employee.setPassword("password");
+        employee.setPositionName("position");
+        employee.setUser(com);
+
+        var x = this.macPropertyInitializer
+                .addElement(employee)
+                .addElement(employee.getUser())
+                .processAll();
+
+        var y = this.encryptionPropertyInitializer
+                .addElement(employee)
+                .addElement(employee.getUser())
+                .processAll();
+
+        return ResponseEntity.ok(com);
     }
 }
