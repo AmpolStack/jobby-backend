2f760b517f10464dca96c11b8f8e268b7ba747c0
Amapola <htrsanbl@gmail.com>
2025-10-04 18:45:57 -0500

    feat(business.jpa.mapper): Domain to JpaEntities mappers are included

---
+++ business/src/main/java/com/jobby/business/infrastructure/persistence/jpa/mappers/JpaCityMapper.java
@@ -0,0 +1,11 @@
+package com.jobby.business.infrastructure.persistence.jpa.mappers;
+
+import com.jobby.business.domain.City;
+import com.jobby.business.infrastructure.persistence.jpa.entities.JpaCityEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = { JpaCountryMapper.class})
+public interface JpaCityMapper {
+    City toDomain(JpaCityEntity jpaCityEntity);
+    JpaCityEntity toJpa(City city);
+}
