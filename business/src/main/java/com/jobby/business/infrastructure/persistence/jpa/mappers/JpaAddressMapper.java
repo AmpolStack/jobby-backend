400ba5925cd1fe84f82170b5865449b1cfdba6b8
Amapola <htrsanbl@gmail.com>
2025-10-06 15:10:33 -0500

    refactor(business.jpa.mapper): create new ignore sentences in mapping

--- business/src/main/java/com/jobby/business/infrastructure/persistence/jpa/mappers/JpaAddressMapper.java
+++ business/src/main/java/com/jobby/business/infrastructure/persistence/jpa/mappers/JpaAddressMapper.java
@@ -3,9 +3,11 @@ package com.jobby.business.infrastructure.persistence.jpa.mappers;
 import com.jobby.business.domain.Address;
 import com.jobby.business.infrastructure.persistence.jpa.entities.JpaAddressEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {JpaCityMapper.class})
 public interface JpaAddressMapper {
     Address toDomain(JpaAddressEntity jpaAddressEntity);
+    @Mapping(target = "valueSearchable", ignore = true)
     JpaAddressEntity toJpa(Address address);
 }
