7b899355a5b3882e0e96182bc21b4bc5c7b058c0
Amapola <htrsanbl@gmail.com>
2025-10-07 22:56:07 -0500

    feat(BusinessApplicationEventService): add service

---
+++ business/src/main/java/com/jobby/business/application/services/business/BusinessApplicationEventService.java
@@ -0,0 +1,20 @@
+package com.jobby.business.application.services.business;
+
+import com.jobby.business.domain.entities.Business;
+import com.jobby.business.domain.ports.in.CreateBusinessEvent;
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BusinessApplicationEventService {
+    private final CreateBusinessEvent createBusinessEvent;
+
+    public BusinessApplicationEventService(CreateBusinessEvent createBusinessEvent) {
+        this.createBusinessEvent = createBusinessEvent;
+    }
+
+    public Result<Business, Error> createBusiness(Business business) {
+        return this.createBusinessEvent.execute(business);
+    }
+}
--- business/src/main/java/com/jobby/business/infrastructure/adapters/in/messaging/CreateBusinessEventConsumer.java
+++ business/src/main/java/com/jobby/business/infrastructure/adapters/in/messaging/CreateBusinessEventConsumer.java
@@ -1,9 +1,8 @@
 package com.jobby.business.infrastructure.adapters.in.messaging;
 
-import com.jobby.business.domain.ports.out.BusinessRepository;
+import com.jobby.business.application.services.business.BusinessApplicationEventService;
 import com.jobby.business.infrastructure.adapters.in.messaging.mappers.SchemaBusinessMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -12,18 +11,17 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CreateBusinessEventConsumer {
     private final SchemaBusinessMapper mapper;
-    private final BusinessRepository readBusinessRepository;
+    private final BusinessApplicationEventService businessApplicationEventService;
 
-    public CreateBusinessEventConsumer(SchemaBusinessMapper mapper, @Qualifier("read") BusinessRepository readBusinessRepository) {
+    public CreateBusinessEventConsumer(SchemaBusinessMapper mapper, BusinessApplicationEventService businessApplicationEventService) {
         this.mapper = mapper;
-        this.readBusinessRepository = readBusinessRepository;
+        this.businessApplicationEventService = businessApplicationEventService;
     }
 
     @KafkaListener(topics = "com.jobby.messaging.event.business.created.v1", groupId = "com.jobby.business.consumer.created")
     public void waitingForInserts(@Payload com.jobby.messaging.schemas.Business business){
         var mapped = this.mapper.toDomain(business);
-        var saved = this.readBusinessRepository.save(mapped);
-
+        var saved = this.businessApplicationEventService.createBusiness(mapped);
         if(saved.isFailure()){
             log.error("Error in inserting business {}",mapped);
         }
