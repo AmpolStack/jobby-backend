7be60ae12db682da7255dec207058b62e2b5c1ff
Amapola <htrsanbl@gmail.com>
2025-10-07 21:37:31 -0500

    refactor: Update controller

--- business/src/main/java/com/jobby/business/infrastructure/adapters/in/rest/BusinessController.java
+++ business/src/main/java/com/jobby/business/infrastructure/adapters/in/rest/BusinessController.java
@@ -1,11 +1,9 @@
 package com.jobby.business.infrastructure.adapters.in.rest;
 
-import com.jobby.business.domain.ports.in.CreateBusinessUseCase;
+import com.jobby.business.application.services.business.BusinessCommandService;
 import com.jobby.business.domain.ports.in.GetBusinessByAddressValueUseCase;
-import com.jobby.business.infrastructure.adapters.in.messaging.mappers.SchemaBusinessMapper;
 import com.jobby.business.infrastructure.adapters.in.rest.dto.CreateBusinessDto;
 import com.jobby.business.infrastructure.adapters.in.rest.mappers.CreateBusinessMapper;
-import com.jobby.domain.ports.MessagingPublisher;
 import com.jobby.infraestructure.response.definition.ApiResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +13,22 @@ import org.springframework.web.bind.annotation.*;
 public class BusinessController {
     private final CreateBusinessMapper createBusinessMapper;
     private final ApiResponseMapper apiResponseMapper;
-    private final MessagingPublisher messagingPublisher;
-    private final SchemaBusinessMapper schemaBusinessMapper;
     private final GetBusinessByAddressValueUseCase getBusinessByAddressValueUseCase;
-    private final CreateBusinessUseCase createBusinessUseCase;
 
-    public BusinessController(CreateBusinessMapper createBusinessMapper, ApiResponseMapper apiResponseMapper, MessagingPublisher messagingPublisher, SchemaBusinessMapper schemaBusinessMapper, GetBusinessByAddressValueUseCase getBusinessByAddressValueUseCase, CreateBusinessUseCase createBusinessUseCase) {
+    private final BusinessCommandService businessCommandService;
+
+    public BusinessController(CreateBusinessMapper createBusinessMapper, ApiResponseMapper apiResponseMapper, GetBusinessByAddressValueUseCase getBusinessByAddressValueUseCase, BusinessCommandService businessCommandService) {
         this.createBusinessMapper = createBusinessMapper;
-        this.schemaBusinessMapper = schemaBusinessMapper;
         this.apiResponseMapper = apiResponseMapper;
-        this.messagingPublisher = messagingPublisher;
         this.getBusinessByAddressValueUseCase = getBusinessByAddressValueUseCase;
-        this.createBusinessUseCase = createBusinessUseCase;
+        this.businessCommandService = businessCommandService;
     }
 
 
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody CreateBusinessDto businessDto){
         var business = this.createBusinessMapper.toDomain(businessDto);
-        var resp = this.createBusinessUseCase.execute(business);
-
-        resp.fold(
-                (onSuccess) -> {
-                        var schemaBusiness = this.schemaBusinessMapper.toSchema(onSuccess);
-                        this.messagingPublisher.publishAsync("com.jobby.messaging.event.business.created.v1", schemaBusiness);
-                    },
-                (onFailure) -> {
-
-                    }
-                );
-
+        var resp = this.businessCommandService.createBusiness(business);
         return apiResponseMapper.map(resp);
     }
 
