bb1152141b558e2f27f5633a43be903f93738244
Amapola <htrsanbl@gmail.com>
2025-10-10 17:26:12 -0500

    feat: the mongo and jpa pipeline is added

---
+++ business/src/main/java/com/jobby/business/infrastructure/common/JpaBusinessPipelinePersistProcess.java
@@ -0,0 +1,49 @@
+package com.jobby.business.infrastructure.common;
+
+import com.jobby.business.domain.entities.Business;
+import com.jobby.business.infrastructure.persistence.jpa.entities.JpaBusinessEntity;
+import com.jobby.business.infrastructure.persistence.jpa.mappers.JpaBusinessMapper;
+import com.jobby.business.infrastructure.secure.SecurityStrategyImplementer;
+import com.jobby.business.infrastructure.secure.SecurityStrategyReverter;
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.error.ErrorType;
+import com.jobby.domain.mobility.error.Field;
+import com.jobby.domain.mobility.result.Result;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class JpaBusinessPipelinePersistProcess implements
+        AfterPersistProcess<Business, JpaBusinessEntity>,
+        BeforePersistProcess<Business, JpaBusinessEntity>
+{
+    private final JpaBusinessMapper jpaBusinessMapper;
+    private final SecurityStrategyReverter<JpaBusinessEntity> securityStrategyReverter;
+    private final SecurityStrategyImplementer<JpaBusinessEntity> securityStrategyImplementer;
+
+    public JpaBusinessPipelinePersistProcess(JpaBusinessMapper jpaBusinessMapper, SecurityStrategyReverter<JpaBusinessEntity> securityStrategyReverter, SecurityStrategyImplementer<JpaBusinessEntity> securityStrategyImplementer) {
+        this.jpaBusinessMapper = jpaBusinessMapper;
+        this.securityStrategyReverter = securityStrategyReverter;
+        this.securityStrategyImplementer = securityStrategyImplementer;
+    }
+
+    @Override
+    public Result<Business, Error> use(Optional<JpaBusinessEntity> jpaBusinessOptional) {
+        if(jpaBusinessOptional.isEmpty()){
+            return Result.failure(ErrorType.USER_NOT_FOUND,
+                    new Field("business", "entity not foud"));
+        }
+
+        var jpaBusiness = jpaBusinessOptional.get();
+        return this.securityStrategyReverter.revert(jpaBusiness)
+                .map(v -> this.jpaBusinessMapper.toDomain(jpaBusiness));
+    }
+
+    @Override
+    public Result<JpaBusinessEntity, Error> use(Business business) {
+        var mapped = this.jpaBusinessMapper.toJpa(business);
+        return this.securityStrategyImplementer.apply(mapped)
+                .map(v -> mapped);
+    }
+}
---
+++ business/src/main/java/com/jobby/business/infrastructure/common/MongoBusinessPipelinePersistProcess.java
@@ -0,0 +1,51 @@
+package com.jobby.business.infrastructure.common;
+
+import com.jobby.business.domain.entities.Business;
+import com.jobby.business.infrastructure.persistence.jpa.entities.JpaBusinessEntity;
+import com.jobby.business.infrastructure.persistence.mongo.entities.MongoBusinessEntity;
+import com.jobby.business.infrastructure.persistence.mongo.mappers.MongoBusinessMapper;
+import com.jobby.business.infrastructure.secure.SecurityStrategyImplementer;
+import com.jobby.business.infrastructure.secure.SecurityStrategyReverter;
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.error.ErrorType;
+import com.jobby.domain.mobility.error.Field;
+import com.jobby.domain.mobility.result.Result;
+
+import java.util.Optional;
+
+public class MongoBusinessPipelinePersistProcess implements
+        AfterPersistProcess<Business, MongoBusinessEntity>,
+        BeforePersistProcess<Business, MongoBusinessEntity>
+{
+
+    private final MongoBusinessMapper mongoBusinessMapper;
+    private final SecurityStrategyReverter<MongoBusinessEntity> securityStrategyReverter;
+    private final SecurityStrategyImplementer<MongoBusinessEntity> securityStrategyImplementer;
+
+    public MongoBusinessPipelinePersistProcess(MongoBusinessMapper mongoBusinessMapper,
+                                               SecurityStrategyReverter<MongoBusinessEntity> securityStrategyReverter,
+                                               SecurityStrategyImplementer<MongoBusinessEntity> securityStrategyImplementer) {
+        this.mongoBusinessMapper = mongoBusinessMapper;
+        this.securityStrategyReverter = securityStrategyReverter;
+        this.securityStrategyImplementer = securityStrategyImplementer;
+    }
+
+    @Override
+    public Result<Business, Error> use(Optional<MongoBusinessEntity> mongoBusinessOptional) {
+        if(mongoBusinessOptional.isEmpty()){
+            return Result.failure(ErrorType.USER_NOT_FOUND,
+                    new Field("business", "entity not foud"));
+        }
+
+        var jpaBusiness = mongoBusinessOptional.get();
+        return this.securityStrategyReverter.revert(jpaBusiness)
+                .map(v -> this.mongoBusinessMapper.toDomain(jpaBusiness));
+    }
+
+    @Override
+    public Result<MongoBusinessEntity, Error> use(Business infra) {
+        var mapped = this.mongoBusinessMapper.toDocument(infra);
+        return this.securityStrategyImplementer.apply(mapped)
+                .map(v -> mapped);
+    }
+}
