5b1bb297053e79e99660be8232589fe766b55b50
Amapola <htrsanbl@gmail.com>
2025-10-14 01:24:11 -0500

    refactor(SpringDataMongoBusinessRepository): Add new queries

--- business/src/main/java/com/jobby/business/infrastructure/persistence/mongo/repositories/SpringDataMongoBusinessRepository.java
+++ business/src/main/java/com/jobby/business/infrastructure/persistence/mongo/repositories/SpringDataMongoBusinessRepository.java
@@ -3,7 +3,10 @@ package com.jobby.business.infrastructure.persistence.mongo.repositories;
 import com.jobby.business.infrastructure.persistence.mongo.entities.MongoBusinessEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface SpringDataMongoBusinessRepository extends MongoRepository<MongoBusinessEntity, Integer> {
+    Optional<MongoBusinessEntity> findById(int id);
+    boolean existsByName(String name);
 }
