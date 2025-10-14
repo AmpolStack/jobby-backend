b5fc9ef489c45c2e708526c46a62432e4e47a73d
Amapola <htrsanbl@gmail.com>
2025-10-14 03:33:25 -0500

    feat(BusinessRepositories): The repositories for write and readonly are included

---
+++ business/src/main/java/com/jobby/business/domain/ports/out/WriteOnlyBusinessRepository.java
@@ -0,0 +1,9 @@
+package com.jobby.business.domain.ports.out;
+
+import com.jobby.business.domain.entities.Business;
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
+
+public interface WriteOnlyBusinessRepository {
+    Result<Business, Error> save(Business business);
+}
