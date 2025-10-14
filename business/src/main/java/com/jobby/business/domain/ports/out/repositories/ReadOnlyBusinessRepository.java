14a55acf5dfcf9d4914c1bedd49009bc117a2c71
Amapola <htrsanbl@gmail.com>
2025-10-14 17:10:14 -0500

    feat(business.repositories): add new methods

--- business/src/main/java/com/jobby/business/domain/ports/out/repositories/ReadOnlyBusinessRepository.java
+++ business/src/main/java/com/jobby/business/domain/ports/out/repositories/ReadOnlyBusinessRepository.java
@@ -8,4 +8,5 @@ public interface ReadOnlyBusinessRepository {
     Result<Business, Error> findById(int id);
     Result<Boolean, Error> existByUsername(String name);
     Result<Void, Error> save(Business business);
+    Result<Void, Error> update(Business business);
 }
