a3ddb7d7b27951f28e1115bd52dd3400c071a62e
Amapola <htrsanbl@gmail.com>
2025-10-13 23:54:13 -0500

    feat(TransactionHandlerAutoConfiguration): Add persistenceTransactionHandler

--- shared/src/main/java/com/jobby/infraestructure/autoconfiguration/TransactionHandlerAutoConfiguration.java
+++ shared/src/main/java/com/jobby/infraestructure/autoconfiguration/TransactionHandlerAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.jobby.infraestructure.autoconfiguration;
 
 import com.jobby.infraestructure.common.transaction.TransactionHandler;
+import com.jobby.infraestructure.repository.transaction.PersistenceTransactionHandler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +12,11 @@ public class TransactionHandlerAutoConfiguration {
     @ConditionalOnMissingBean
     public TransactionHandler transactionHandler() {
         return new TransactionHandler();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public PersistenceTransactionHandler persistenceTransactionHandler() {
+        return new PersistenceTransactionHandler();
     }
 }
