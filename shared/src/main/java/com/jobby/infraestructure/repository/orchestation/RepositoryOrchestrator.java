0100eda9b4240d76d0454ec0879e84420c82e2e5
Amapola <htrsanbl@gmail.com>
2025-10-15 23:59:22 -0500

    feat(RepositoryOrchestation): define operation exist

--- shared/src/main/java/com/jobby/infraestructure/repository/orchestation/RepositoryOrchestrator.java
+++ shared/src/main/java/com/jobby/infraestructure/repository/orchestation/RepositoryOrchestrator.java
@@ -10,4 +10,5 @@ public interface RepositoryOrchestrator<Infra,Domain> {
     Result<Domain, Error> selection(Supplier<Optional<Infra>> supplier);
     <T> Result<T, Error> operation(Supplier<T> supplier);
     <T> Result<T, Error> modification(Domain domain, Function<Infra, T> function);
+    Result<Void, Error> exist(Supplier<Boolean> supplier, String name);
 }
