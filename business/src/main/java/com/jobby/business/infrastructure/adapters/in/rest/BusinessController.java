1f830c6eecedc30b195dc1a913ece199a3a1accf
Amapola <htrsanbl@gmail.com>
2025-10-20 20:48:22 -0500

    feat: The controller implements the CQRS calling strategy

--- business/src/main/java/com/jobby/business/infrastructure/adapters/in/rest/BusinessController.java
+++ business/src/main/java/com/jobby/business/infrastructure/adapters/in/rest/BusinessController.java
@@ -1,6 +1,8 @@
 package com.jobby.business.infrastructure.adapters.in.rest;
 
-import com.jobby.business.application.services.business.BusinessApplicationService;
+import com.jobby.business.application.services.BusinessCommandExecutor;
+import com.jobby.business.application.services.BusinessQueryExecutor;
+import com.jobby.business.application.useCase.*;
 import com.jobby.business.infrastructure.adapters.in.rest.dto.CreateBusinessDto;
 import com.jobby.business.infrastructure.adapters.in.rest.dto.UpdateBusinessPicturesDto;
 import com.jobby.business.infrastructure.adapters.in.rest.dto.UpdateBusinessPropertiesDto;
@@ -14,45 +16,52 @@ import org.springframework.web.bind.annotation.*;
 public class BusinessController {
     private final CreateBusinessMapper createBusinessMapper;
     private final ApiResponseMapper apiResponseMapper;
-    private final BusinessApplicationService businessApplicationService;
+    private final BusinessCommandExecutor businessCommandExecutor;
+    private final BusinessQueryExecutor businessQueryExecutor;
+
 
     public BusinessController(CreateBusinessMapper createBusinessMapper,
-                              ApiResponseMapper apiResponseMapper,
-                              BusinessApplicationService businessApplicationService) {
+                              ApiResponseMapper apiResponseMapper, BusinessCommandExecutor businessCommandExecutor, BusinessQueryExecutor businessQueryExecutor) {
         this.createBusinessMapper = createBusinessMapper;
         this.apiResponseMapper = apiResponseMapper;
-        this.businessApplicationService = businessApplicationService;
+        this.businessCommandExecutor = businessCommandExecutor;
+        this.businessQueryExecutor = businessQueryExecutor;
     }
 
 
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody CreateBusinessDto businessDto){
         var business = this.createBusinessMapper.toDomain(businessDto);
-        var resp = this.businessApplicationService.createBusiness(business);
+        var command = new BusinessDefaultCreateCommand(business);
+        var resp = this.businessCommandExecutor.execute(command);
         return apiResponseMapper.map(resp);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<?> deleteById(@PathVariable int id){
-        var resp = this.businessApplicationService.deleteById(id);
+        var command = new BusinessDeleteByIdCommand(id);
+        var resp = this.businessCommandExecutor.execute(command);
         return apiResponseMapper.map(resp);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable int id){
-        var resp = this.businessApplicationService.getBusiness(id);
+        var query = new BusinessQueryById(id);
+        var resp = this.businessQueryExecutor.execute(query);
         return apiResponseMapper.map(resp);
     }
 
     @PatchMapping("/{id}/updateProperties")
     public ResponseEntity<?> updateProperties(@PathVariable int id, @RequestBody UpdateBusinessPropertiesDto dto){
-        var resp = this.businessApplicationService.updateProperties(id, dto.getName(), dto.getDescription());
+        var command = new UpdateBusinessPropertiesCommand(id, dto.getName(), dto.getDescription());
+        var resp = this.businessCommandExecutor.execute(command);
         return apiResponseMapper.map(resp);
     }
 
     @PatchMapping("/{id}/updatePictures")
     public ResponseEntity<?> updatePics(@PathVariable int id, @RequestBody UpdateBusinessPicturesDto dto){
-        var resp = this.businessApplicationService.updatePictures(id, dto.getBannerImageUrl(), dto.getProfileImageUrl());
+        var command = new UpdateBusinessPicturesCommand(id, dto.getBannerImageUrl(), dto.getProfileImageUrl());
+        var resp = this.businessCommandExecutor.execute(command);
         return apiResponseMapper.map(resp);
     }
 }
