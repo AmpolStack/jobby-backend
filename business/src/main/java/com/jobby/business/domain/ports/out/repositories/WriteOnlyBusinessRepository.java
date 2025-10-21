6020757ce67a607f9f6aa75066d326965412e07f
Amapola <htrsanbl@gmail.com>
2025-10-20 19:53:40 -0500

    feat(WriteOnlyBusinessRepoisotiry): add new queries

--- business/src/main/java/com/jobby/business/domain/ports/out/repositories/WriteOnlyBusinessRepository.java
+++ business/src/main/java/com/jobby/business/domain/ports/out/repositories/WriteOnlyBusinessRepository.java
@@ -8,5 +8,11 @@ public interface WriteOnlyBusinessRepository {
     Result<Business, Error> save(Business business);
     Result<Business, Error> update(Business business);
     Result<Business, Error> findById(int id);
-    Result<Void, Error> delete(Business business);
+    Result<Void, Error> delete(int id);
+    
+    // Nuevos métodos específicos para updates optimizados
+    Result<Business, Error> updatePictures(int id, String bannerImageUrl, String profileImageUrl);
+    Result<Business, Error> updateName(int id, String name);
+    Result<Business, Error> updateDescription(int id, String description);
+    Result<Business, Error> updateNameAndDescription(int id, String name, String description);
 }
