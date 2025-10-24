e73ed2bb43e6b1c2089f40fed7a83ecd0f60788e
Amapola <htrsanbl@gmail.com>
2025-10-24 01:06:39 -0500

    feat: add new queries for businessSetQuery

---
+++ business/src/main/java/com/jobby/business/application/useCase/BusinessSetQueryByCityId.java
@@ -0,0 +1,23 @@
+package com.jobby.business.application.useCase;
+
+import com.jobby.business.domain.entities.Business;
+import com.jobby.business.domain.operations.BusinessSetQuery;
+import com.jobby.business.domain.ports.out.repositories.ReadOnlyBusinessRepository;
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
+
+import java.util.Set;
+
+public class BusinessSetQueryByCityId extends BusinessSetQuery {
+    private final int cityId;
+
+
+    public BusinessSetQueryByCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    @Override
+    public Result<Set<Business>, Error> execute(ReadOnlyBusinessRepository repository) {
+        return repository.findByCityId(cityId);
+    }
+}
