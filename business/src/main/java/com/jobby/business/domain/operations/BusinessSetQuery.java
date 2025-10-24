b2a2a4319fcc7544fb4e4be5b89194d42575a671
Amapola <htrsanbl@gmail.com>
2025-10-24 01:06:10 -0500

    feat(BusinessSetQuery): add class

---
+++ business/src/main/java/com/jobby/business/domain/operations/BusinessSetQuery.java
@@ -0,0 +1,12 @@
+package com.jobby.business.domain.operations;
+
+import com.jobby.business.domain.entities.Business;
+import com.jobby.business.domain.ports.out.repositories.ReadOnlyBusinessRepository;
+import com.jobby.domain.mobility.error.Error;
+import com.jobby.domain.mobility.result.Result;
+
+import java.util.Set;
+
+public abstract class BusinessSetQuery {
+    public abstract Result<Set<Business>, Error> execute(ReadOnlyBusinessRepository repository);
+}
