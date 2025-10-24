76e3458c152f109ee9d8f9e777f26127b3726da7
Amapola <htrsanbl@gmail.com>
2025-10-24 01:13:27 -0500

    feat: delete the domain.ports.in

--- business/src/main/java/com/jobby/business/domain/ports/in/DeleteBusinessCommand.java
+++
@@ -1,8 +0,0 @@
-package com.jobby.business.domain.ports.in;
-
-import com.jobby.domain.mobility.error.Error;
-import com.jobby.domain.mobility.result.Result;
-
-public interface DeleteBusinessCommand {
-    Result<Void, Error> delete(int id);
-}
