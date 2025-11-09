5d1d259277d1f3ac07334bb938c0d18ba6cd5643
Amapola <htrsanbl@gmail.com>
2025-11-09 15:25:51 -0500

    feat: add mapping method

--- business/src/main/java/com/jobby/business/infrastructure/persistence/business/mongo/mappers/MongoBusinessMapper.java
+++ business/src/main/java/com/jobby/business/infrastructure/persistence/business/mongo/mappers/MongoBusinessMapper.java
@@ -4,8 +4,11 @@ import com.jobby.business.domain.entities.Business;
 import com.jobby.business.infrastructure.persistence.business.mongo.entities.MongoBusinessEntity;
 import org.mapstruct.Mapper;
 
+import java.util.Set;
+
 @Mapper(componentModel = "spring", uses = { MongoAddressMapper.class })
 public interface MongoBusinessMapper {
     Business toDomain(MongoBusinessEntity mongoBusinessEntity);
+    Set<Business> toDomain(Set<MongoBusinessEntity> mongoBusinessEntity);
     MongoBusinessEntity toDocument(Business business);
 }
