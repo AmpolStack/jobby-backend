24643bddf6a950e21c116a5b678270b25eebf27a
Amapola <htrsanbl@gmail.com>
2025-11-25 23:34:54 -0500

    feat: The secured composers were are separed

--- shared/src/main/java/com/jobby/infraestructure/autoconfiguration/PasswordTransformerAutoConfiguration.java
+++ shared/src/main/java/com/jobby/infraestructure/autoconfiguration/PasswordTransformerAutoConfiguration.java
@@ -3,15 +3,11 @@ package com.jobby.infraestructure.autoconfiguration;
 
 import com.jobby.domain.ports.hashing.HashingService;
 import com.jobby.infraestructure.security.SecuredPasswordTransformer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass(HashingService.class)
-@ConditionalOnBean(HashingService.class)
 public class PasswordTransformerAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
--- shared/src/main/java/com/jobby/infraestructure/autoconfiguration/PropertyTransformerAutoConfiguration.java
+++ shared/src/main/java/com/jobby/infraestructure/autoconfiguration/PropertyTransformerAutoConfiguration.java
@@ -5,24 +5,13 @@ import com.jobby.domain.configurations.MacConfig;
 import com.jobby.domain.ports.SafeResultValidator;
 import com.jobby.domain.ports.encrypt.EncryptionService;
 import com.jobby.domain.ports.hashing.mac.MacService;
+import com.jobby.infraestructure.security.SecuredPropertyComposer;
 import com.jobby.infraestructure.security.SecuredPropertyTransformer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass({EncryptConfig.class,
-        EncryptionService.class,
-        MacService.class,
-        MacConfig.class,
-        SafeResultValidator.class})
-@ConditionalOnBean({EncryptConfig.class,
-        EncryptionService.class,
-        MacService.class,
-        MacConfig.class,
-        SafeResultValidator.class})
 public class PropertyTransformerAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
@@ -36,6 +25,22 @@ public class PropertyTransformerAutoConfiguration {
         return new SecuredPropertyTransformer(encryptionService,
                 macService,
                 encryptConfig,
+                macConfig,
+                safeResultValidator);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SecuredPropertyComposer securedPropertyComposer(
+            EncryptionService encryptionService,
+            EncryptConfig encryptConfig,
+            MacService macService,
+            MacConfig macConfig,
+            SafeResultValidator safeResultValidator
+    ){
+        return new SecuredPropertyComposer(encryptionService,
+                encryptConfig,
+                macService,
                 macConfig,
                 safeResultValidator);
     }
