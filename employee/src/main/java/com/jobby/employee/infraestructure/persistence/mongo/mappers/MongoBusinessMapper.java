c33544fdd3e7c3ad10af82ccb9eb4a1305ec8947
Amapola <htrsanbl@gmail.com>
2025-11-25 23:36:06 -0500

    refactor: update the mongo mappers

--- employee/src/main/java/com/jobby/employee/infraestructure/persistence/mongo/mappers/MongoBusinessMapper.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/persistence/mongo/mappers/MongoBusinessMapper.java
@@ -4,7 +4,7 @@ import com.jobby.employee.domain.model.Business;
 import com.jobby.employee.infraestructure.persistence.mongo.entities.MongoBusinessEntity;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = { MongoBusinessMapper.class })
+@Mapper(componentModel = "spring", uses = { MongoAddressMapper.class })
 public interface MongoBusinessMapper {
     Business toDomain(MongoBusinessEntity mongoBusinessEntity);
     MongoBusinessEntity toDocument(Business business);
--- employee/src/main/java/com/jobby/employee/infraestructure/persistence/mongo/mappers/MongoUserMapper.java
+++ employee/src/main/java/com/jobby/employee/infraestructure/persistence/mongo/mappers/MongoUserMapper.java
@@ -7,10 +7,18 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MongoUserMapper {
+
+    // Leemos directamente los campos planos (cifrados) de Mongo
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "phone", source = "phone")
+    @Mapping(target = "firstName", source = "firstName")
     User toDomain(MongoUserEntity mongoUserEntity);
-    @Mapping(target = "firstNameSearchable", ignore = true)
-    @Mapping(target = "lastNameSearchable", ignore  = true)
-    @Mapping(target = "emailSearchable", ignore = true)
-    @Mapping(target = "phoneSearchable", ignore = true)
+
+    // Escribimos el valor lógico directamente en los campos planos
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "phone", source = "phone")
+    @Mapping(target = "firstName", source = "firstName")
     MongoUserEntity toDocument(User user);
 }
