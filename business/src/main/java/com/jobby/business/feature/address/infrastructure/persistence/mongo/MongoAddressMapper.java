6385888283edb9c44739e5154b7264fbb2ad801f
Amapola <htrsanbl@gmail.com>
2025-11-26 02:41:04 -0500

    refactor(Address.MongoEntity): Replace the securized entity to plain properties

--- business/src/main/java/com/jobby/business/feature/address/infrastructure/persistence/mongo/MongoAddressMapper.java
+++ business/src/main/java/com/jobby/business/feature/address/infrastructure/persistence/mongo/MongoAddressMapper.java
@@ -9,16 +9,6 @@ import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring", uses = {MongoCityMapper.class})
 public interface MongoAddressMapper {
-    @Mapping(target = "value", source = "value.rawValue")
     Address toDomain(MongoAddressEntity mongoAddressEntity);
-
-    @Mapping(target = "value", source = "value", qualifiedByName = "toSecuredProperty")
     MongoAddressEntity toDocument(Address address);
-
-    @Named("toSecuredProperty")
-    default SecuredProperty toSecuredProperty(String value){
-        var secured = new SecuredProperty();
-        secured.setRawValue(value);
-        return secured;
-    }
 }
